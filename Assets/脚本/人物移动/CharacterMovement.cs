7efa0faf2b36e646dbed87b88dccf64a33f01cf2
mashimarokumo <2732946269@qq.com>
2023-07-08 13:18:52 +0800

    基本机制实现与地形震动

--- Assets/脚本/人物移动/CharacterMovement.cs
+++ Assets/脚本/人物移动/CharacterMovement.cs
@@ -2,10 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
+
     public float speed;
     private Rigidbody2D rb;
     void Start()
